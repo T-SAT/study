0eeeadbc3a851dad936b50ee619d05e1fa314122
h-genki <goforbroke.0301@gmail.com>
2017-03-27 15:39:45 +0900

    L3GD20 copy

--- tki/tki.ino
+++ tki/tki.ino
@@ -1,9 +1,95 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Wire.h>
 
+// Connect L3GD20 with SDA (A4), SCL (A5)
+
+const byte L3GD20_ADDR = B1101010;  // SA0 = GND
+//const byte L3GD20_ADDR = B1101011;// SA0 = VDD_IO
+
+const byte L3GD20_WHOAMI = 0x0f;
+const byte L3GD20_CTRL1 = 0x20;
+const byte L3GD20_CTRL2 = 0x21;
+const byte L3GD20_CTRL3 = 0x22;
+const byte L3GD20_CTRL4 = 0x23;
+const byte L3GD20_CTRL5 = 0x24;
+const byte L3GD20_X_L = 0x28;
+const byte L3GD20_X_H = 0x29;
+const byte L3GD20_Y_L = 0x2A;
+const byte L3GD20_Y_H = 0x2B;
+const byte L3GD20_Z_L = 0x2C;
+const byte L3GD20_Z_H = 0x2D;
+
+void L3GD20_write(byte reg, byte val)
+{
+  Wire.beginTransmission(L3GD20_ADDR);
+  Wire.write(reg);
+  Wire.write(val);
+  Wire.endTransmission();
+}
+
+byte L3GD20_read(byte reg)
+{
+  byte ret = 0;
+  // request the registor
+  Wire.beginTransmission(L3GD20_ADDR);
+  Wire.write(reg);
+  Wire.endTransmission();
+
+  // read
+  Wire.requestFrom((unsigned int)L3GD20_ADDR, 1);
+
+  while (Wire.available()) {
+    ret = Wire.read();
+  }
+
+  return ret;
+}
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {}
+
+  Wire.begin();
+
+  Serial.println(L3GD20_read(L3GD20_WHOAMI), HEX); // should show D4
+
+  L3GD20_write(L3GD20_CTRL1, B00001111);
+  //   |||||||+ X axis enable
+  //   ||||||+- Y axis enable
+  //   |||||+-- Z axis enable
+  //   ||||+--- PD: 0: power down, 1: active
+  //   ||++---- BW1-BW0: cut off 12.5[Hz]
+  //   ++------ DR1-DR0: ODR 95[HZ]
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  short X, Y, Z;
+  float x, y, z;
 
+  X = L3GD20_read(L3GD20_X_H);
+  x = X = (X << 8) | L3GD20_read(L3GD20_X_L);
+  Y = L3GD20_read(L3GD20_Y_H);
+  y = Y = (Y << 8) | L3GD20_read(L3GD20_Y_L);
+  Z = L3GD20_read(L3GD20_Z_H);
+  z = Z = (Z << 8) | L3GD20_read(L3GD20_Z_L);
+
+  x *= 0.00875; // +-250dps
+  //x *= 0.0175;// +-500dps
+  //x *= 0.07;  // +-2000dps
+  y *= 0.00875; // +-250dps
+  z *= 0.00875; // +-250dps
+
+  Serial.print(X);    // X axis (reading)
+  Serial.print("\t");
+  Serial.print(Y);    // Y axis (reading)
+  Serial.print("\t");
+  Serial.print(Z);    // Z axis (reading)
+  Serial.print("\t");
+  Serial.print(x);    // X axis (deg/sec)
+  Serial.print("\t");
+  Serial.print(y);    // Y axis (deg/sec)
+  Serial.print("\t");
+  Serial.println(z);  // Z axis (deg/sec)
+
+  delay(10);
 }
+
